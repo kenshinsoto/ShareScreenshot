bc4e5d2a740b4a6a72d4e6b09c6442c768f5c3de
kristian <christian.soto@banregio.com>
2020-04-14 15:03:40 -0500

    Se modifica vista para incluir boton de captura

--- app/src/main/java/com/kristianconk/appscreenshot/MainActivity.kt
+++ app/src/main/java/com/kristianconk/appscreenshot/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.kristianconk.appscreenshot
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.kristianconk.appscreenshot.databinding.ActivityMainBinding
+import com.kristianconk.screenshare.Share
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnScreenshare.setOnClickListener {
+            Share.shareScreenshot(
+                this,
+                "te comparto mi captura",
+                binding.root
+            )
+        }
     }
 }
